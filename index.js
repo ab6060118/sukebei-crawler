5ed92cb2475bed9599e08335131f73e9dd25d457
DaubaKao <ab6060118@gmail.com>
2019-04-15 16:23:50 +0000

    update

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var http = require('https');
+var request = require('request');
 var fs = require('fs');
 var Crawler = require("crawler");
 var config = require('./config.js')
 var admin = require('firebase-admin');
 var credential =  require('./credential.json')
 var hash = require('object-hash');
+var thumb = require('node-thumbnail').thumb;
 var data = {}
 
 admin.initializeApp({
@@ -16,15 +17,19 @@ admin.initializeApp({
 var sukebeiBucket = admin.storage().bucket()
 var sukebeiCollection = admin.firestore().collection('sukebeiPosts')
 
+if (!fs.existsSync('./temp')){
+  fs.mkdirSync('./temp');
+}
+
 var c = new Crawler({
-  maxConnections : 10,
+  maxConnections : 1,
   callback: function(error, res, done) {
     console.log(data, 'down');
     done()
   }
 });
 
-function queryFc2(post) {
+function queryFc2(hashName, post) {
   if(!post) {
     console.log('the post is missing.');
     return
@@ -39,7 +44,9 @@ function queryFc2(post) {
       else {
         var $ = res.$
         var imgs = []
+        var promises = []
 
+        if($('section.detail a').attr('href')) imgs.push($('section.detail a').attr('href'))
         $('section.sample_images a').each(function(index, e) {
           var href = $(e).attr('href')
           if(href && typeof href === 'string' && href.includes('storage') && !href.includes('thumb') && !href.includes('gif')) imgs.push(href);
@@ -49,8 +56,6 @@ function queryFc2(post) {
           if(href && typeof href === 'string' && href.includes('storage') && !href.includes('thumb') && !href.includes('gif')) imgs.push(href);
         });
 
-        console.log(hash(post));
-
         imgs.forEach(function(url, index) {
           var filename = url.split('/').pop()
 
@@ -58,35 +63,59 @@ function queryFc2(post) {
 
           var filepath = 'temp/' + filename
           var file = fs.createWriteStream(filepath);
-          var request = http.get(url, function(response) {
-            var stream = response.pipe(file);
-            stream.on('finish', function() {
-              sukebeiBucket.upload(filepath, {
-                destination: 'sukebeiPost/' + post.no + '.' + filename,
-              }).then(function(res) {
-                console.log(res);
-                fs.unlink(filepath, function(err) {
-                  if (err) {
-                    console.error(err)
-                    return
-                  }
+
+          var promise = new Promise(function(resolve) {
+            request(url).pipe(file).on('close', function() {
+              thumb({
+                suffix: '',
+                source: filepath,
+                destination: 'temp',
+                width: 540,
+                quiet: true,
+                overwrite: true,
+              }).then(function() {
+                sukebeiBucket.upload(filepath, {
+                  destination: 'sukebeiPost/' + post.no + '.' + filename,
+                }).then(function(res) {
+                  if(!post.images) post.images = []
+                  var { bucket, name } = res[1]
+                  post.images.push(`https://firebasestorage.googleapis.com/v0/b/${bucket}/o/${encodeURIComponent(name)}?alt=media`)
+                  resolve(true)
+                  fs.unlink(filepath, function(err) {
+                    if (err) {
+                      console.error(err)
+                      return
+                    }
+                  })
+                }).catch(function(err) {
+                  console.log(err);
                 })
-              }).catch(function(err) {
+              }, function(err) {
                 console.log(err);
               })
             })
-          });
+          })
+
+          promises.push(promise)
+        })
+
+        Promise.all(promises).then(function() {
+          write_data(hashName, post)
+          done()
+        }, function(error) {
+          console.log(error);
         })
       }
-      done()
     }
   })
 }
 
-function write_data(data) {
-  var doc = sukebeiCollection.doc(hash(data))
+function write_data(name, data) {
+  var doc = sukebeiCollection.doc(name)
 
-  doc.set(data)
+  doc.set(data).then(function() {
+    console.log(`Write post ${name} success`);
+  })
 }
 
 function main() {
@@ -99,7 +128,6 @@ function main() {
       else{
         var $ = res.$
         $('.success').each(function(index, e) {
-          if(index !== 0) return
           var reg = /\d{6,}/
           var stringArr = $(e).find('td:nth-child(2) a').text().match(reg)
           var magnet = $(e).find('i.fa-magnet').parent().attr('href')
@@ -107,21 +135,18 @@ function main() {
 
           if(!stringArr[0] || !magnet) { return }
 
-          // var doc = sukebeiCollection.doc(stringArr[0] + '#' + time)
+          var post = { name: stringArr.input, no: parseInt(stringArr[0]), magnet: magnet, time: time }
+          var name = hash(post)
+          var doc = sukebeiCollection.doc(name)
 
-          // doc.get().then(function(snap) {
-            // if(!snap.exists) {
-              // doc.set({
-                // name: stringArr.input,
-                // no: parseInt(stringArr[0]),
-                // magnet: magnet,
-                // time: time
-              // })
-
-              // queryFc2(stringArr[0])
-            // }
-          // })
-          queryFc2({ name: stringArr.input, no: parseInt(stringArr[0]), magnet: magnet, time: time })
+          doc.get().then(snap => {
+            if(!snap.exists) {
+              queryFc2(name, post)
+            }
+            else {
+              console.log(`Post ${name} is exists`);
+            }
+          })
         });
       }
       done()
@@ -130,10 +155,8 @@ function main() {
 }
 
 main()
-// console.log('search');
 
-// setInterval(function() {
-  // main()
-  // console.log('search');
-// }, 1000*60*config.interval)
+setInterval(function() {
+  main()
+}, 1000*60*config.interval)
 
